a0f1f1b9ea24a0b07cf4fdccb177c2b386a404eb
Sun <sixsixsix516@163.com>
2020-09-21 17:59:24 +0800

    kafka

---
+++ 深入理解kafka/src/main/java/com/sixsixsix516/SunApp.java
@@ -0,0 +1,12 @@
+package com.sixsixsix516;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sun 2020/9/21 11:34
+ */
+@SpringBootApplication
+public class SunApp {
+
+
+}
